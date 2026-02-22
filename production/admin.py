38df9a2da4d802e3e2c5ec1c1f5887495d58fd26
karigosammy5 <karigosammy5@gmail.com>
2026-02-22 23:05:36 +0300

    Enhanced Admin interface with date hierarchy and series filters

--- production/admin.py
+++ production/admin.py
@@ -3,7 +3,14 @@ from .models import ProductionTask
 
 @admin.register(ProductionTask)
 class ProductionTaskAdmin(admin.ModelAdmin):
-    list_display = ('series', 'lot_number', 'model_name', 'daily_target', 'total_units', 'prd_customer')
-    search_fields = ('lot_number', 'model_name', 'prd_customer')
-list_filter = ('series', 'production_date')
-date_hierarchy = 'production_date'
+    # This defines the columns you see in the list
+    list_display = ('production_date', 'series', 'lot_number', 'model_name', 'daily_target')
+    
+    # THIS creates the calendar timeline bar at the top
+    date_hierarchy = 'production_date' 
+    
+    # THIS creates the "Series" and "Date" sidebar filters on the right
+    list_filter = ('series', 'production_date') 
+    
+    # This adds the search bar
+    search_fields = ('lot_number', 'model_name')
