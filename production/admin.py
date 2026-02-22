40dd51c65fceb6063a5a2506dc5b01abb4218a41
karigosammy5 <karigosammy5@gmail.com>
2026-02-22 22:23:39 +0300

    added production_date field and date_hierarchy to admin

--- production/admin.py
+++ production/admin.py
@@ -3,5 +3,7 @@ from .models import ProductionTask
 
 @admin.register(ProductionTask)
 class ProductionTaskAdmin(admin.ModelAdmin):
-    list_display = ('lot_number', 'model_name', 'daily_target', 'total_units', 'prd_customer')
+    list_display = ('series', 'lot_number', 'model_name', 'daily_target', 'total_units', 'prd_customer')
     search_fields = ('lot_number', 'model_name', 'prd_customer')
+list_filter = ('series', 'production_date')
+date_hierarchy = 'production_date'
--- production/models.py
+++ production/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 class ProductionTask(models.Model):
+   # Choices for the dropdown
+    SERIES_CHOICES = [
+        ('N', 'N-Series (Light Duty)'),
+        ('F', 'F-Series (Medium Duty)'),
+    ]
+    
+    # New Field
+    series = models.CharField(
+        max_length=1, 
+        choices=SERIES_CHOICES, 
+        default='F'
+    )
+   # The "When" - adding the date field
+    production_date = models.DateField(default=timezone.now)
+
     lot_number = models.CharField(max_length=20)
     model_name = models.CharField(max_length=100)
     ckd_arrival_iea = models.CharField(max_length=100, blank=True)
