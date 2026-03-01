334555c92f862ea963e2d876ce9fd9ee1dcfc2d1
karigosammy5 <karigosammy5@gmail.com>
2026-03-01 20:06:49 +0300

    final submission of capstone project. fixed bugs

--- production/views.py
+++ production/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# Make sure your actual model name is imported here!
+from .models import ProductionTask 
 
-# Create your views here.
+def dashboard_view(request):
+    # Fetch the tasks
+    tasks = ProductionTask.objects.all().order_by('series', 'production_date') 
+    
+    # Send them to the template
+    return render(request, 'production/dashboard.html', {'tasks': tasks})
